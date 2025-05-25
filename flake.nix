3431bdafead1971235280b1d927e79288cc82072
Ayush Paudel <paudel.ayushp@gmail.com>
2025-05-26 06:11:34 +1000

    final now?

--- flake.nix
+++ flake.nix
@@ -1,32 +1,29 @@
 {
- 	description = "The beginning to the end";
+  description = "all to water all to dirt, breathe now and forget it all";
 
- 	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-	};
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    # Home manager
+  };
 
-	outputs = { self, nixpkgs, ... } @ inputs: 
-	let
-		system = "x86_64-linux";
-		
-		pkgs = import nixpkgs {
-			inherit system;
-			
-			config = {
-				allowUnfree = true;
-			};
-		};
-	in
-	{
-		nixosConfigurations = {
-			myNixos = nixpkgs.lib.nixosSystem {
-				specialArgs = { inherit system; };
-				modules = [
-					./etc/nixos/configuration.nix
-					./etc/nixos/hardware-configuration.nix
-				];
-			};
-		};
-	};
-
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      dev-env-pmx = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        # > Our main nixos configuration file <
+        modules = [./machines/configuration.nix];
+      };
+    };
+  };
 }
