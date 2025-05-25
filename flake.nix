402afe98dd42830d5cfa6661edf7fa6abb58c701
Ayush Paudel <paudel.ayushp@gmail.com>
2025-05-26 05:20:17 +1000

    os flake?

--- flake.nix
+++ flake.nix
@@ -1,15 +1,22 @@
 {
-  description = "The beginning to the end";
+ 	description = "The beginning to the end";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+ 	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+	};
 
-  outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs, ... } @ inputs: 
+	let
+		root-save = /etc/nixos;
+	in
+	{
+		nixosConfigurations.nixos  = nixpkgs.lib.nixosSystem {
+			specialArgs = { inherit inputs; };
+			modules = [
+				.root-save/configuration.nix
+				.root-save/hardware-configuration.nix
+			];
+		};
+	};
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
-  };
 }
