e57793cbc6a3fb9c396dba4540b2c8659de0a91a
Ayush Paudel <paudel.ayushp@gmail.com>
2025-05-26 12:25:03 +1000

    change xterm

--- pkgs/gui/gui.nix
+++ pkgs/gui/gui.nix
@@ -1,9 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-  services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  	services.xserver.enable = true;
 
+	services.xserver.displayManager.sddm.enable = true;
+	services.xserver.desktopManager.plasma6.enable = true;
+
+	services.xrdp.enable = true;
+	services.xrdp.defaultWindowManager = "xterm";
+	services.xrdp.openFirewall = true;
   # Optional: additional GUI packages
 }
