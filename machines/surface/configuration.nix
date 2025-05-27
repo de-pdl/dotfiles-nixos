52e082370a10f1d4d6a6e7995c57df55d71f82fb
Ayush <paudel.ayushp@gmail.com>
2025-05-27 20:24:18 +1000

    uh trial run

--- machines/surface/configuration.nix
+++ machines/surface/configuration.nix
@@ -64,7 +64,7 @@
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
 
   programs.hyprland.enable = true;
 
