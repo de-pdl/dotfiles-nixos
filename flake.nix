d89bc17df918680549b3eae465f6410ff05d0c90
Ayush Paudel <paudel.ayushp@gmail.com>
2025-05-27 14:38:56 +1000

    overhaul pt 2.2

--- flake.nix
+++ flake.nix
@@ -4,34 +4,41 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
+    #home manager
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    
+
   };
 
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs   = import nixpkgs {
-        inherit system;
-        overlays = [
-          (final: prev: {
-            guiApps = import ./pkgs/gui/apps/default.nix { pkgs = prev; };
-          })
-        ];
-      };
-    in {
-      nixosConfigurations = {
-      	dev-env-pmx = nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit pkgs; };
+  outputs = { self, nixpkgs, home-manager, flake-utils, ... }:
+    flake.utils.lib.eachDefaultSystem (system: 
+    	let
+      		pkgs   = import nixpkgs {
+			inherit system;
+			config.allowUnfree = true;
+		};
+	in {
+		homeConfigurations.test = home-manager.lib.homeManagerConfiguration {
+			inherit pkgs;
+			modules  = [ ./home/test.nix];
+	}}) // 
 
+     {
+     nixosConfigurations.dev-env-pmx = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
           modules = [
-            ./machines/dev-env-pmx/configuration.nix
-            ./pkgs/cli               # <-- cli/default.nix
-            ./pkgs/gui/env/hyprland
+		./machines/dev-env-pmx/configuration.nix
+	    	./machines/dev-env-pmx/hardware-configuration.nix
+		home-manager.nixosModules.home-manager 
+	    {
+	    	home-manager.useUserPackages = true;
+		home-manager.useGlobalPkgs = true;
+		home-manager.users.test = import ./home/test.nix;
+            }
           ];
         };
       };
-
-      # if you also want to expose packages:
-      packages.${system}.guiApps = pkgs.guiApps;
-    };
 }
---
+++ home/test.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "test";
+  home.homeDirectory = "/home/test";
+
+  home.stateVersion = "24.05";  # Match your current NixOS version
+
+  programs.hyprland.enable = true;
+
+  # Symlink Hyprland config from your repo to ~/.config/hypr/hyprland.conf
+  xdg.configFile."hypr/hyprland.conf".source = ./dotfiles/test/hypr/hyprland.conf;
+
+  # Set Hyprland config path environment variable
+  home.sessionVariables = {
+    HYPRLAND_CONFIG_DIR = "${config.xdg.configHome}/hypr";
+  };
+
+  # Optional: common user tools
+  programs.zsh.enable = true;
+  home.packages = with pkgs; [
+    git
+    neovim
+    kitty
+  ];
+}
+
