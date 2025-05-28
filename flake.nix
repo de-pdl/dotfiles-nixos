37db255260af47a3c1be9f916ba56b905d33ec9e
Ayush <paudel.ayushp@gmail.com>
2025-05-29 01:29:28 +1000

    t1

--- flake.nix
+++ flake.nix
@@ -4,14 +4,48 @@
   inputs = {
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
-    #home manager
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    
     #hardware-config
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
+    #system pkgs
+
+    #desktop environment
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";	# main window manager
+    hyprpanel.url = "github:Jas-SinghFSU/HyprPanel"; 				# bar {all the lil gadgets ig on the top}
+    nixvim.url = "github:nix-community/nixvim";					# neovim for nix through flake
+
+
+    hyprspace = {								# workspace manager
+      url = "github:KZDKM/Hyprspace";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    # essentials
+    sops-nix.url = "github:Mic92/sops-nix"; 					# secrets
+    search-nixos-api.url = "github:anotherhadi/search-nixos-api";		# nix api
+    
+    home-manager = {                               				#home-manager {usr pkg}
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+   
+    # person style
+    stylix.url = "github:danth/stylix"; 					# themeing framework
+    #apple-fonts.url = "github:Lyndeno/apple-fonts.nix"; 			# fonts
+
+    # server items
+    #nixarr.url = "github:rasmus-kirk/nixarr"; 					# media-server
+    #anyrun.url = "github:fufexan/anyrun/launch-prefix"; 			# malware-sandbox
+
+    # app-specific
+    nixcord.url = "github:kaylorben/nixcord"; 					# discord cli
+    zen-browser.url = "github:0xc000022070/zen-browser-flake"; 			# brower
+    spicetify-nix = {								# spotify cli
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+        
+
 
   };
 
